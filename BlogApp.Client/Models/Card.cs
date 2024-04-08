8723ea55a24a7285e89f16a076eaab1778f9f8f9
Sami Afzal <egtsami@gmail.com>
2024-04-09 00:38:48 +0500

    Route Paging

---
+++ BlogApp.Client/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace BlogApp.Client.Models
+{
+    // Card.cs
+    public class Card
+    {
+        public int Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ReadMoreUrl { get; set; }
+    }
+
+}
